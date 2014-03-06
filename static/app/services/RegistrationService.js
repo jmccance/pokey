7bfcf5aa97ac25eb8d9ff3a47da036138064e57e
Joel McCance <joelm@slalom.com>
2014-03-06 09:18:27 -0600

    Adding some notes about RegistrationService.

---
+++ static/app/services/RegistrationService.js
@@ -0,0 +1,6 @@
+// TODO Implement
+// Provides an Angular service to handle user registration.
+// isRegistered(): boolean - check if we are currently registered
+// getName(): string - gets the value of the name cookie, or null if not set
+// register(name: string=undefined) - register. If no name is specified, pass in the name cookie
+//                                    value
