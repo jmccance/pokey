2fc0bb4354a33e7444c5cb95bcb3d34e764f478c
Joel McCance <joelm@slalom.com>
2014-03-12 22:27:59 -0500

    POC of the histogram functionality.
    Desperately crude, but it shows how it can work.

--- static/app/room/RoomCtrl.js
+++ static/app/room/RoomCtrl.js
@@ -1,7 +1,9 @@
 define([
+  'underscore',
+
   // Mix-ins
   'highcharts'
-], function () {
+], function (_) {
   'use strict';
 
   return [
@@ -22,6 +24,61 @@ define([
         $scope.$apply(function () {
           $scope.room = room;
         });
+
+        if (room.isRevealed) {
+          var tally = _.chain(room.members)
+              .countBy(function (member) {
+                if (!_.isUndefined(member.estimate)) {
+                  return member.estimate.hours;
+                }
+              })
+              .omit(undefined)
+              .value();
+
+          var categories = [];
+          var data = [];
+
+          for (var i = 1; i <= 12; ++i) {
+            categories.push(i + 'h');
+            data.push(tally[i] ? tally[i] : 0);
+          }
+
+          // TODO Implement an EstimationChart abstraction layer.
+          // 1. update(data) - Takes a collection of estimates and updates the chart from them.
+          // 2. clear() - Clears the chart, perhaps hiding it as well
+          // 3. A constructor to handle naming the chart and its axes and specifying the range of
+          //    series.
+          $('[data-node-name="chart"]')
+              .show()
+              .highcharts({
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: null
+            },
+            legend: {
+              enabled: false
+            },
+            xAxis: {
+//              categories: ['1h', '2h', '3h', '4h', '5h', '6h']
+              categories: categories
+            },
+            yAxis: {
+              title: {
+                text: '# of votes'
+              },
+              tickInterval: 1
+            },
+            series: [
+              {
+                data: data
+              }
+            ]
+          });
+        } else {
+          $('[data-node-name="chart"]').hide();
+        }
       });
 
       if (!pokeyService.isRegistered()) {
@@ -46,36 +103,6 @@ define([
        */
       $scope.revealEstimates = function () {
         pokeyService.showEstimates();
-
-        // TODO Implement an EstimationChart abstraction layer.
-        // 1. update(data) - Takes a collection of estimates and updates the chart from them.
-        // 2. clear() - Clears the chart, perhaps hiding it as well
-        // 3. A constructor to handle naming the chart and its axes and specifying the range of
-        //    series.
-        $('[data-node-name="chart"]').highcharts({
-          chart: {
-            type: 'column'
-          },
-          title: {
-            text: null
-          },
-          legend: {
-            enabled: false
-          },
-          xAxis: {
-            categories: ['1h', '2h', '3h', '4h', '5h', '6h']
-          },
-          yAxis: {
-            title: {
-              text: '# of votes'
-            }
-          },
-          series: [
-            {
-              data: [0, 2, 1, 4, 3, 2]
-            }
-          ]
-        });
       };
 
       /**
